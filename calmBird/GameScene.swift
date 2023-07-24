a78af12fdd03f708f00b23f1903ff520ee236a2b
Salih Kocatürk <kocaturksalih8@gmail.com>
2023-07-24 18:42:04 +0300

    added gravity, bird effected by gravity.

--- calmBird/GameScene.swift
+++ calmBird/GameScene.swift
@@ -13,10 +13,22 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     var bird = SKSpriteNode()
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
     override func didMove(to view: SKView) {
-        
-        
-        
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        // bird
+     
+        bird = childNode(withName: "bird") as! SKSpriteNode
+        let  birdtexture = SKTexture(imageNamed: "bird")
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdtexture.size().width / 13)
+        bird.physicsBody?.affectedByGravity = true
+        bird.physicsBody?.mass = 0.5
+       
+     
     }
         
         func touchDown(atPoint pos : CGPoint) {
